dc21beb22978b93e8a6a1051b9ec6b7678216877
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2020-10-08 20:34:42 -0700

    Added the Firebase Admin keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCmts66uEUxCkkjrtW3ssG-pGZvhyJZfIU",
+  authDomain: "fhelandingpage.firebaseapp.com",
+  databaseURL: "https://fhelandingpage.firebaseio.com",
+  projectId: "fhelandingpage",
+  storageBucket: "fhelandingpage.appspot.com",
+  messagingSenderId: "1045838672768",
+  appId: "1:1045838672768:web:34790e08ecd45d0ffbc765",
+  measurementId: "G-RFNYGV66CV"
 };
 
 firebase.initializeApp(config);
