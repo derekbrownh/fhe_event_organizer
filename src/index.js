71522c0624bfaf788a8a8be37835c0114b5f97e6
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2020-10-09 18:48:09 -0600

    --force firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { App } from "./App";
+import {SignIn, SignUp} from "./auth"
+import {BrowserRouter, Route} from "react-router-dom"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<BrowserRouter>
+    <div>
+        <Route path ="/" component = {SignIn}/>
+        <Route path ="/signup" component = {SignUp}/>
+        <Route exact path ="/app" component = {App}/>
+    </div>
+</BrowserRouter>
+, document.getElementById("root"));
 
 serviceWorker.unregister();
