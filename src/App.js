1cfbde757b250548943ffb9242739a6271e7f246
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2020-10-10 19:27:56 -0600

    added save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from "react";
 import { AppBarFun } from "./components/AppBar";
 import { auth, db, snapshotToArray } from "./firebase";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 export function App(props) {
   const [user, setUser] = useState(null);
+  const [test, setTest] = useState(null)
+
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((u) => {
@@ -17,9 +21,33 @@ export function App(props) {
     return unsubscribe;
   }, [props.history]);
 
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user.uid)
+        .collection("test")
+        .onSnapshot((snapshot) => {
+          const test = snapshotToArray(snapshot);
+          setTest(test);
+        });
+    }
+  }, [user]);
 
-  return <div>
+
+
+  return (<div>
     <AppBarFun user={user}></AppBarFun>
     <h1 style={{textAlign: 'center'}}>Hello World!</h1>;
-    </div>
+    <TextField
+            placeholder="Email"
+            fullWidth="true"
+            value={test}
+            onChange={e => {
+              setTest(e.target.value);
+            }}
+          />
+    <Button color="inherit">
+        Testing
+      </Button>
+    </div>)
 }
